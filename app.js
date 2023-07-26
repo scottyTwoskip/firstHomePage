fa45d3175c4180874f3d82bea847eae2304ffb7b
scottyTwoskip <scottyeansor@gmail.com>
2023-07-26 10:25:58 -0700

    added html and css

---
+++ app.js
@@ -0,0 +1,7 @@
+let input = {
+    //make a function that is called that saves each id in an onbject and then presents it in entry flexbox permaently.
+    // id: function generateNewId(),
+    date: null,
+    time: null,
+    entry: ''
+}
