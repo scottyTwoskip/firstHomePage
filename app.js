ea9244afed67d39215984a0c43c4ebf696ab64f0
scottyTwoskip <scottyeansor@gmail.com>
2023-08-08 11:56:02 -0700

    started date app function. 1. Created variables for html. 2. crreated save function that stores data in object and working storing to local

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-let input = {
-    //make a function that is called that saves each id in an onbject and then presents it in entry flexbox permaently.
-    // id: function generateNewId(),
-    date: null,
-    time: null,
-    entry: ''
-}
+//set variables for form, inputs
+const datesForm = document.querySelector('.entryContainer')
+const datesDateInput = document.querySelector('#dateInput')
+const datesTimeInput = document.querySelector('#timeInput')
+const datesEntryInput = document.querySelector('#entryInput')
+
+// data 
+let datesData = []
+
+//left off here
+//set where files come from and how
+
+
+datesForm.addEventListener('submit', (i) => {
+    i.preventDefault();
+    const objectDate = datesDateInput.value
+    const objectTime = datesTimeInput.value
+    const objectEntry = datesEntryInput.value
+
+    //object goes here where data is stored
+    datesDataObject = {
+        date: objectDate,
+        time: objectTime,
+        entry: objectEntry,
+    }
+    //pushing to local storage here
+    datesData.push(datesDataObject)
+    console.log(datesDataObject)
+    localStorage.setItem('datesData', JSON.stringify(datesData))
+    function displayData() {
+        localStorage.setItem('datesData', datesData.value)
+    }
+})
+
