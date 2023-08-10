8f456c3badf7fee39bbbd750a4dc5eed151187e8
scottyTwoskip <scottyeansor@gmail.com>
2023-08-10 07:19:02 -0700

    added to locacl storage. need to work on added delete button to local

--- app.js
+++ app.js
@@ -1,34 +1,55 @@
-//set variables for form, inputs
-const datesForm = document.querySelector('.entryContainer')
-const datesDateInput = document.querySelector('#dateInput')
-const datesTimeInput = document.querySelector('#timeInput')
-const datesEntryInput = document.querySelector('#entryInput')
+// Set variables for form, inputs
+const datesForm = document.querySelector('.entryContainer');
+const datesDateInput = document.querySelector('#dateInput');
+const datesTimeInput = document.querySelector('#timeInput');
+const datesEntryInput = document.querySelector('#entryInput');
+const savedDateAndTime = document.querySelector('#savedDateAndTime');
+const savedEntry = document.querySelector('#savedEntry');
+const savedList = document.querySelector('.savedList');
 
-// data 
-let datesData = []
+// Data
+//if no datesData in local storage it turns into an array is what the code below means
+let datesData = JSON.parse(localStorage.getItem('datesData')) || [];
 
-//left off here
-//set where files come from and how
+datesForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
+    const objectDate = datesDateInput.value;
+    const objectTime = datesTimeInput.value;
+    const objectEntry = datesEntryInput.value;
 
-datesForm.addEventListener('submit', (i) => {
-    i.preventDefault();
-    const objectDate = datesDateInput.value
-    const objectTime = datesTimeInput.value
-    const objectEntry = datesEntryInput.value
-
-    //object goes here where data is stored
-    datesDataObject = {
+    if (objectDate.trim() === '' || objectTime.trim() === '' || objectEntry.trim() === '') {
+        alert('Please fill out a Time, Date and Entry')
+        // this return here is what causes it to return early so its not added to local storage
+        return
+    }
+    // Object goes here where data is stored
+    const datesDataObject = {
         date: objectDate,
         time: objectTime,
         entry: objectEntry,
-    }
-    //pushing to local storage here
-    datesData.push(datesDataObject)
-    console.log(datesDataObject)
-    localStorage.setItem('datesData', JSON.stringify(datesData))
-    function displayData() {
-        localStorage.setItem('datesData', datesData.value)
-    }
-})
+    };
 
+    // Pushing to local storage here
+    datesData.push(datesDataObject);
+    localStorage.setItem('datesData', JSON.stringify(datesData));
+
+    // Display data
+    displayData();
+});
+
+function displayData() {
+    savedList.innerHTML = ''; // Clear previous data
+
+    for (const entry of datesData) {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            <p>Date & Time: ${entry.date} ${entry.time}</p>
+            <p> Description: ${entry.entry}</p>
+        `;
+        savedList.appendChild(listItem);
+    }
+}
+
+// Initial display
+displayData();
