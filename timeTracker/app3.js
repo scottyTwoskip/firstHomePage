299a78a4a611576c9931296ff71173bd7e11ac8d
scottyTwoskip <scottyeansor@gmail.com>
2023-09-05 15:13:45 -0700

    finished timer function, going to add text box settings

--- timeTracker/app3.js
+++ timeTracker/app3.js
@@ -1,6 +1,5 @@
 //global variables
-const timeElement = document.querySelector('.time, .timerContainer')
-//buttons
+const timeElement = document.querySelector('.time')
 const startBtn = document.getElementById('start')
 const pauseBtn = document.getElementById('pause')
 const restartBtn = document.getElementById('restart')
@@ -9,6 +8,9 @@ let seconds = 0
 let interval = null
 
 //event listners
+startBtn.addEventListener('click', start)
+pauseBtn.addEventListener('click', pause)
+restartBtn.addEventListener('click', reset)
 
 //fucntion that ticks up and udpates timer
 function timer() {
@@ -17,7 +19,28 @@ function timer() {
     let mins = Math.floor((seconds - (hrs * 3600)) / 60)
     let secs = seconds % 60
 
+    if (secs < 10) { secs = '0' + secs }
+    if (mins < 10) { mins = '0' + mins }
+    if (hrs < 10) { hrs = '0' + hrs }
+
     timeElement.innerText = `${hrs}:${mins}:${secs}`
 }
 
-timer()
+//button functions here
+function start() {
+    if (interval) {
+        return
+    }
+    interval = setInterval(timer, 1000)
+}
+
+function pause() {
+    clearInterval(interval)
+    interval = null
+}
+
+function reset() {
+    stop()
+    seconds = 0
+    timeElement.innerText = '00:00:00'
+}
